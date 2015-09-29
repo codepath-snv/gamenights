6072c7b4b26b5be29f0db0296d8fdd9b71135e68
gideong <gideong@yahoo-inc.com>
2015-09-28 20:04:31 -0700

    OMG I seriously broke xcode.

---
+++ Gamenights/GroupModel.swift
@@ -0,0 +1,37 @@
+//
+//  GroupModel.swift
+//  Gamenights
+//
+//  Created by Gideon Goodwin on 9/28/15.
+//  Copyright © 2015 T-West. All rights reserved.
+//
+import UIKit
+
+class GroupModel: NSObject {
+    var name: String?
+    init(dictionary: NSDictionary?) {
+    }
+
+    func save(onDone: (error: NSError?) -> Void) {
+        let pfObj = PFObject(className: "Group")
+        pfObj["name"] = name
+        pfObj.saveInBackgroundWithBlock{(succeeded: Bool, error: NSError?) -> Void in
+            if (!succeeded) {
+                print("Failed to save a Group")
+            }
+            onDone(error: error)
+        }
+    }
+
+    func loadAll(onDone: (results: [AnyObject]?, error: NSError?) -> Void) {
+        let query = PFQuery(className: "Group")
+        query.findObjectsInBackgroundWithBlock{ (objects: [AnyObject]?, error: NSError?) -> Void in
+            if (error == nil) {
+            }
+        }
+    }
+
+    func delete() {
+    }
+
+}
