d692ac15148de061897055334441ce9e3bb2b252
gideong <gideong@yahoo-inc.com>
2015-09-29 12:57:58 -0700

    SessionModel

--- Gamenights/SessionModel.swift
+++ Gamenights/SessionModel.swift
@@ -1,5 +1,5 @@
 //
-//  GroupGameModel.swift
+//  SessionModel.swift
 //  Gamenights
 //
 //  Created by Gideon Goodwin on 9/28/15.
@@ -7,33 +7,39 @@
 //
 import UIKit
 
-class GroupGameModel: NSObject {
+class SessionModel: NSObject {
     var pfObjId: String? = nil
-    var parentGroupId: String? = nil
-    var name: String? = "New Game"
-    var notes: String? = ""
+    var parentGroupGameId: String? = nil
+    var date: String? = nil
+    var players: String? = nil
+    var winner: String? = nil
+    var notes: String? = nil
 
-    init(parentGroupId: String?, pfObj: PFObject?) {
-        self.parentGroupId = parentGroupId
+    init(parentGroupGameId: String?, pfObj: PFObject?) {
+        self.parentGroupGameId = parentGroupGameId
         if let pfObj = pfObj {
             pfObjId = pfObj.objectId
-            name = pfObj["name"] as? String
+            date = pfObj[""] as? String
+            players = pfObj[""] as? String
+            winner = pfObj[""] as? String
             notes = pfObj["notes"] as? String
         }
     }
 
     // for both create and update
     func save(onDone: (error: NSError?) -> Void) {
-        let pfObj = PFObject(className: "GroupGame")
+        let pfObj = PFObject(className: "Session")
         if (pfObjId != nil) {
             pfObj.objectId = pfObjId
         }
-        pfObj["name"] = name
+        pfObj["date"] = date
+        pfObj["players"] = players
+        pfObj["winner"] = winner
         pfObj["notes"] = notes
-        pfObj["parentGroupId"] = parentGroupId
+        pfObj["parentGroupGameId"] = parentGroupGameId
         pfObj.saveInBackgroundWithBlock{(succeeded: Bool, error: NSError?) -> Void in
             if (!succeeded) {
-                print("Failed to save a GroupGame")
+                print("Failed to save a Session")
             } else {
                 self.pfObjId = pfObj.objectId
             }
@@ -41,39 +47,38 @@ class GroupGameModel: NSObject {
         }
     }
 
-    class func loadAllByParentId(parentGroupId: String!, onDone: (results: [GroupGameModel]?, error: NSError?) -> Void) {
-        let query = PFQuery(className: "GroupGame")
+    class func loadAllByParentId(parentGroupGameId: String!, onDone: (results: [SessionModel]?, error: NSError?) -> Void) {
+        let query = PFQuery(className: "Session")
         let cb: PFQueryArrayResultBlock? = {(objects: [PFObject]?, error: NSError?) -> Void in
             if (error != nil) {
                 onDone(results: nil, error: error)
                 return
             }
-            var results = [GroupGameModel]()
+            var results = [SessionModel]()
             for object in objects! {
-                results.append(GroupGameModel(parentGroupId: parentGroupId, pfObj: object))
+                results.append(SessionModel(parentGroupGameId: parentGroupGameId, pfObj: object))
             }
             onDone(results: results, error: nil);
         }
         query.findObjectsInBackgroundWithBlock(cb)
     }
 
-    class func loadOne(pfObjId: String!, onDone: (result: GroupGameModel?, error: NSError?) -> Void) {
-        let query = PFQuery(className: "GroupGame")
+    class func loadOne(pfObjId: String!, onDone: (result: SessionModel?, error: NSError?) -> Void) {
+        let query = PFQuery(className: "Session")
         query.getObjectInBackgroundWithId(pfObjId, block: { (pfResult: PFObject?, error: NSError?) -> Void in
             if (error != nil) {
                 onDone(result: nil, error: error)
                 return
             }
-            let parentGroupId = pfResult?["parentGroupId"] as? String
-            let model = GroupGameModel(parentGroupId: parentGroupId, pfObj: pfResult)
+            let parentGroupGameId = pfResult?["parentGroupGameId"] as? String
+            let model = SessionModel(parentGroupGameId: parentGroupGameId, pfObj: pfResult)
             onDone(result: model, error: nil)
         })
     }
 
     func deleteModel(onDone: (succeeded: Bool, error: NSError?) -> Void) {
         assert(pfObjId != nil, "Need an objectID if you want to delete a specific model from Parse")
-        // TODO: what about dangling child nodes? meh
-        let query = PFQuery(className: "GroupGame")
+        let query = PFQuery(className: "Session")
         query.getObjectInBackgroundWithId(pfObjId!, block: { (pfResult: PFObject?, error: NSError?) -> Void in
             if (error != nil) {
                 onDone(succeeded: false, error: error)
@@ -84,7 +89,7 @@ class GroupGameModel: NSObject {
     }
 
     // convenience method to find a certain model in the list by id
-    class func findById(models: [GroupGameModel]?, id: NSString?) -> GroupGameModel? {
+    class func findById(models: [SessionModel]?, id: NSString?) -> SessionModel? {
         if (models == nil || id == nil) {
             return nil
         }
