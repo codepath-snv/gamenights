7a13f74b0275dc6e1562cef1c4a44e6deb8141db
Shawn Zhu <shawn@tumblr.com>
2015-10-01 21:51:15 -0700

    add Constants for use in the app

---
+++ Gamenights/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Gamenights
+//
+//  Created by Shawn Zhu on 10/1/15.
+//  Copyright © 2015 T-West. All rights reserved.
+//
+
+import Foundation
+struct Constants {
+    struct UserDefaults {
+        static let KEY_DEFAULT_GROUP = "defaultGroup"
+    }
+}
